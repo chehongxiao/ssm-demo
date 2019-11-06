ac40d2ccb133801192fe7648f717f50ffe937fd0
chehongxiao <306944255@qq.com>
2019-11-06 14:18:48 +0800

    aop

---
+++ src/main/java/com/css/ssm/util/AopLoggerAspect.java
@@ -0,0 +1,48 @@
+package com.css.ssm.util;
+
+
+import org.apache.log4j.Logger;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+
+public class AopLoggerAspect {
+
+    private static final Logger logger = Logger.getLogger(AopLoggerAspect.class);
+
+    public void doBefore(JoinPoint joinPoint) {
+        String methodName = joinPoint.getSignature().getName();
+        logger.info("Method Name : [" + methodName + "] ---> AOP before ");
+    }
+
+    public void doAfter(JoinPoint joinPoint) {
+        String methodName = joinPoint.getSignature().getName();
+        logger.info("Method Name : [" + methodName + "] ---> AOP after ");
+    }
+
+    public void afterReturn(JoinPoint joinPoint, Object result) {
+        String methodName = joinPoint.getSignature().getName();
+        logger.info("Method Name : [" + methodName + "] ---> AOP after return ,and result is : " + result.toString());
+    }
+
+    public void afterThrowing(JoinPoint joinPoint, Throwable throwable) {
+        String methodName = joinPoint.getSignature().getName();
+        logger.info("Method Name : [" + methodName + "] ---> AOP after throwing ,and throwable message is : " + throwable.getMessage());
+    }
+
+    public Object around(ProceedingJoinPoint joinPoint) {
+        String methodName = joinPoint.getSignature().getName();
+        try {
+            logger.info("Method Name : [" + methodName + "] ---> AOP around start");
+            long startTimeMillis = System.currentTimeMillis();
+            //调用 proceed() 方法才会真正的执行实际被代理的方法
+            Object result = joinPoint.proceed();
+            long execTimeMillis = System.currentTimeMillis() - startTimeMillis;
+            logger.info("Method Name : [" + methodName + "] ---> AOP method exec time millis : " + execTimeMillis);
+            logger.info("Method Name : [" + methodName + "] ---> AOP around end , and result is : " + result.toString());
+            return result;
+        } catch (Throwable te) {
+            logger.error(te.getMessage(),te);
+            throw new RuntimeException(te.getMessage());
+        }
+    }
+}
